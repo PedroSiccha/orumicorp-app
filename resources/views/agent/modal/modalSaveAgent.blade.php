9610dbe7a4ef6fbde4c10844641a074cc45bb7ba
Pedro Siccha <pedro.d.siccha@gmail.com>
2025-01-18 12:09:53 -0500

    Fix create agt

---
+++ resources/views/agent/modal/modalSaveAgent.blade.php
@@ -0,0 +1,88 @@
+<div class="modal inmodal fade" id="modalAgente" tabindex="-1" role="dialog"  aria-hidden="true">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+                <h4 class="modal-title">Nuevo Agente</h4>
+                <small class="font-bold">Registre su nuevo agente</small>
+            </div>
+            <div class="modal-body">
+                <table class="table m-b-xs">
+                    <tbody>
+                        <tr>
+                            <td>
+                                <strong>Código</strong>
+                            </td>
+                            <td>
+                                <input style='font-size: large;' type='text' class='form-control text-success' placeholder="Ingrese su código" id='code'>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <strong>Nombre</strong>
+                            </td>
+                            <td>
+                                <input style='font-size: large;' type='text' class='form-control text-success' placeholder="Ingrese su nombre" id='name'>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <strong>Apellidos</strong>
+                            </td>
+                            <td>
+                                <input style='font-size: large;' type='text' class='form-control text-success' placeholder="Ingrese su apellido" id='lastname'>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <strong>Código Voiso</strong>
+                            </td>
+                            <td>
+                                <input style='font-size: large;' type='text' class='form-control text-success' placeholder="Registro Código Voiso" id='codeVoiso'>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <strong>Correo</strong>
+                            </td>
+                            <td>
+                                <input style='font-size: large;' type='email' class='form-control text-success' placeholder="Ingrese su correo" id='email'>
+                            </td>
+                        </tr>
+                        <tr>
+                              <td>
+                                    <strong>Área</strong>
+                              </td>
+                              <td>
+                                    <select class="form-control m-b" name="account" id="area_id">
+                                          <option>Seleccione su Área</option>
+                                          @foreach($areas as $area)
+                                          <option value = "{{ $area->id }}">{{ $area->name }}</option>
+                                          @endforeach
+                                      </select>
+                              </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                  <strong>Asignar un Rol</strong>
+                            </td>
+                            <td>
+                                  <select class="form-control m-b" name="account" id="rol_id">
+                                        <option>Seleccione su Rol</option>
+                                        @foreach($roles as $rol)
+                                        <option value = "{{ $rol->id }}">{{ $rol->name }}</option>
+                                        @endforeach
+                                    </select>
+                            </td>
+                      </tr>
+                    </tbody>
+                </table>
+            </div>
+
+            <div class="modal-footer">
+                <button class="btn btn-info " type="button" onclick="guardarNuevoAgente('#code', '#name', '#lastname', '#codeVoiso', '#email', '#area_id', '#rol_id', '#modalAgente', '#tabAgente')"><i class="fa fa-save"></i> Guardar</button>
+                <button class="btn btn-default" data-dismiss="modal" type="button"><i class="fa fa-trash"></i> Cancelar</button>
+            </div>
+        </div>
+    </div>
+</div>
