ca362833838041c0f69883993076b4bb6b5c96d0
Pedro Siccha <pedro.d.siccha@gmail.com>
2025-03-30 12:10:48 -0500

    Corrección Filtro por Fecha

--- app/Http/Controllers/StatisticsTodayController.php
+++ app/Http/Controllers/StatisticsTodayController.php
@@ -116,26 +116,105 @@ class StatisticsTodayController extends Controller
      */
     public function filterStatistics(Request $request)
     {
-        $dateInit = DateTime::createFromFormat('m/d/Y', $request->dateInit)->format('Y-m-d');
-        $dateEnd = DateTime::createFromFormat('m/d/Y', $request->dateEnd)->format('Y-m-d');
+        $user_id = Auth::user()->id;
+        $user = User::where('id', $user_id)->first();
+        $roles = $user->getRoleNames()->first();
+
+        $dateInit = DateTime::createFromFormat('d/m/Y', $request->dateInit)->format('Y-m-d');
+        $dateEnd = DateTime::createFromFormat('d/m/Y', $request->dateEnd)->format('Y-m-d');
 
         $currentDate = Carbon::now()->toDateString();
         $currentMonth = Carbon::now()->format('Y-m');
 
         $agent = Agent::where('area_id', $request->area)->first();
 
-        $sales = Sales::join('agents as a', 'sales.agent_id', '=', 'a.id')
-                            ->selectRaw('a.name, a.lastname,
-                                        SUM(CASE WHEN sales.action_id = 4 THEN sales.amount ELSE 0 END) AS total_amount_action_4,
-                                        SUM(sales.amount) AS total_amount_day,
-                                        SUM(sales.amount) AS total_amount_month')
-                            ->addSelect(DB::raw('(SELECT COUNT(*) FROM sales WHERE sales.agent_id = a.id) AS total_sales_day'))
-                            ->addSelect(DB::raw('(SELECT COUNT(*) FROM sales WHERE sales.agent_id = a.id) AS total_sales_month'))
+        if ($roles == 'ADMINISTRADOR') {
+            $sales = Sales::join('agents as a', 'sales.agent_id', '=', 'a.id')
+                            ->selectRaw('
+                                a.name, 
+                                a.lastname,
+                                SUM(CASE WHEN sales.action_id = 1 THEN sales.amount ELSE 0 END) AS total_amount_action_4,
+                                SUM(CASE WHEN sales.action_id = 1 AND DATE(sales.created_at) = ? THEN sales.amount ELSE 0 END) AS total_amount_day,
+                                SUM(CASE WHEN sales.action_id = 1 AND DATE_FORMAT(sales.created_at, "%Y-%m") = ? THEN sales.amount ELSE 0 END) AS total_amount_month
+                            ')
+                            ->addSelect(DB::raw('
+                                (SELECT COUNT(*) FROM sales 
+                                WHERE sales.action_id = 1 
+                                AND DATE(sales.created_at) = ? 
+                                AND sales.agent_id = a.id) AS total_sales_day
+                            '))
+                            ->addSelect(DB::raw('
+                                (SELECT COUNT(*) FROM sales 
+                                WHERE sales.action_id = 1 
+                                AND DATE_FORMAT(sales.created_at, "%Y-%m") = ? 
+                                AND sales.agent_id = a.id) AS total_sales_month
+                            '))
                             ->where('a.area_id', $request->area)
+                            ->where('sales.agent_id', $agent->id)
                             ->whereBetween('sales.date_admission', [$dateInit, $dateEnd])
                             ->groupBy('a.id')
                             ->orderBy('total_amount_day', 'DESC')
+                            ->setBindings([
+                                $currentDate,   // Primer placeholder: DATE(sales.created_at) = ?
+                                $currentMonth,  // Segundo placeholder: DATE_FORMAT(sales.created_at, "%Y-%m") = ?
+                                $currentDate,   // Tercer placeholder: en subselect para total_sales_day
+                                $currentMonth,  // Cuarto placeholder: en subselect para total_sales_month
+                                $request->area, // Quinto placeholder: where 'a.area_id' = ?
+                                $dateInit,      // Sexto placeholder: whereBetween - inicio
+                                $dateEnd        // Séptimo placeholder: whereBetween - fin
+                            ])
                             ->get();
+        } else {
+            $sales = Sales::join('agents as a', 'sales.agent_id', '=', 'a.id')
+                        ->selectRaw('
+                            a.name, 
+                            a.lastname,
+                            SUM(CASE WHEN sales.action_id = 1 THEN sales.amount ELSE 0 END) AS total_amount_action_4,
+                            SUM(CASE WHEN sales.action_id = 1 AND DATE(sales.created_at) = ? THEN sales.amount ELSE 0 END) AS total_amount_day,
+                            SUM(CASE WHEN sales.action_id = 1 AND DATE_FORMAT(sales.created_at, "%Y-%m") = ? THEN sales.amount ELSE 0 END) AS total_amount_month
+                        ')
+                        ->addSelect(DB::raw('
+                            (SELECT COUNT(*) FROM sales 
+                            WHERE sales.action_id = 1 
+                            AND DATE(sales.created_at) = ? 
+                            AND sales.agent_id = a.id) AS total_sales_day
+                        '))
+                        ->addSelect(DB::raw('
+                            (SELECT COUNT(*) FROM sales 
+                            WHERE sales.action_id = 1 
+                            AND DATE_FORMAT(sales.created_at, "%Y-%m") = ? 
+                            AND sales.agent_id = a.id) AS total_sales_month
+                        '))
+                        ->where('a.area_id', $request->area)
+                        ->whereBetween('sales.date_admission', [$dateInit, $dateEnd])
+                        ->groupBy('a.id')
+                        ->orderBy('total_amount_day', 'DESC')
+                        ->setBindings([
+                            $currentDate,   // Primer placeholder: DATE(sales.created_at) = ?
+                            $currentMonth,  // Segundo placeholder: DATE_FORMAT(sales.created_at, "%Y-%m") = ?
+                            $currentDate,   // Tercer placeholder: en subselect para total_sales_day
+                            $currentMonth,  // Cuarto placeholder: en subselect para total_sales_month
+                            $request->area, // Quinto placeholder: where 'a.area_id' = ?
+                            $dateInit,      // Sexto placeholder: whereBetween - inicio
+                            $dateEnd        // Séptimo placeholder: whereBetween - fin
+                        ])
+                        ->get();
+        }
+        
+
+
+        // $sales = Sales::join('agents as a', 'sales.agent_id', '=', 'a.id')
+        //                     ->selectRaw('a.name, a.lastname,
+        //                                 SUM(CASE WHEN sales.action_id = 4 THEN sales.amount ELSE 0 END) AS total_amount_action_4,
+        //                                 SUM(sales.amount) AS total_amount_day,
+        //                                 SUM(sales.amount) AS total_amount_month')
+        //                     ->addSelect(DB::raw('(SELECT COUNT(*) FROM sales WHERE sales.agent_id = a.id) AS total_sales_day'))
+        //                     ->addSelect(DB::raw('(SELECT COUNT(*) FROM sales WHERE sales.agent_id = a.id) AS total_sales_month'))
+        //                     ->where('a.area_id', $request->area)
+        //                     ->whereBetween('sales.date_admission', [$dateInit, $dateEnd])
+        //                     ->groupBy('a.id')
+        //                     ->orderBy('total_amount_day', 'DESC')
+        //                     ->get();
 
         return response()->json(["view"=>view('todayStatistics.components.tabStatistics', compact('sales'))->render()]);
     }
