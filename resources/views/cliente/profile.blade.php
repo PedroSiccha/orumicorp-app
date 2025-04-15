8c898e1fdc838d545b0b3623777dd01f7a0641db
Pedro Siccha <pedro.d.siccha@gmail.com>
2025-04-15 17:02:12 -0500

    Se agregró los nombres y apellidos en las asignaciones

--- resources/views/cliente/profile.blade.php
+++ resources/views/cliente/profile.blade.php
@@ -421,7 +421,7 @@
                                                         <td>{{ $loop->iteration }}</td>
                                                         <td>{{ $listAssignament->agent->name.' '.$listAssignament->agent->lastname ?? 'Sin Agente' }}</td>
                                                         <td>{{ \Carbon\Carbon::parse($listAssignament->date)->format('d/m/Y H:i:s') }}</td>
-                                                        <td>{{ $listAssignament->assignedBy->name.' '.$listAssignament->assignedBy->lastname ?? 'Sin Agente' }}</td>
+                                                        <td>{{ optional($listAssignament->asignadorAgente)->name.' '.optional($listAssignament->asignadorAgente)->lastname ?? 'Sin Agente' }}</td>
                                                         <td>{{ $listAssignament->comment }}</td>
                                                         <td>{{ $listAssignament->descripcion }}</td>
                                                     </tr>
